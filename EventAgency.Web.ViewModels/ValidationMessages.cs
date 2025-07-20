d288d24b2d5a982d5eb06360e4358380938dad04
StefanyDimova <stefany.asenova@gmail.com>
2025-07-20 20:29:33 +0300

    Add validation messages.

--- EventAgency.Web.ViewModels/ValidationMessages.cs
+++ EventAgency.Web.ViewModels/ValidationMessages.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace EventAgency.Web.ViewModels
+﻿namespace EventAgency.Web.ViewModels
 {
     public class ValidationMessages
     {
