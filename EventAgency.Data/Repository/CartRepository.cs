1f0a6e00f05d81dbc28c5869678d035e1b4cfdd9
StefanyDimova <stefany.asenova@gmail.com>
2025-07-22 18:03:32 +0300

    Implement CartRepository and interface.

---
+++ EventAgency.Data/Repository/CartRepository.cs
@@ -0,0 +1,47 @@
+﻿using EventAgency.Data.Models;
+using EventAgency.Data.Repository.Interfaces;
+using EventAgencyFinalProject.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace EventAgency.Data.Repository
+{
+    public class CartRepository : BaseRepository<ApplicationUserProduct, object>, ICartRepository
+    {
+
+        public CartRepository(EventAgencyDbContext dbContext) : base(dbContext)
+        {
+        }
+
+        public bool Exists(string userId, string productId)
+        {
+            return this
+                .GetAllAttached()
+                .Any(ap => ap.ApplicationUserId.ToLower() == (userId.ToLower()) &&
+                                ap.ProductId.ToString().ToLower() == (productId.ToLower()));
+        }
+
+        public Task<bool> ExistsAsync(string userId, string productId)
+        {
+            return this
+               .GetAllAttached()
+               .AnyAsync(ap => ap.ApplicationUserId.ToLower() == (userId.ToLower()) &&
+                               ap.ProductId.ToString().ToLower() == (productId.ToLower()));
+        }
+
+        public ApplicationUserProduct? GetByCompositeKey(string userId, string productId)
+        {
+            return this
+                .GetAllAttached()
+                .SingleOrDefault(ap => ap.ApplicationUserId.ToLower() == (userId.ToLower()) &&
+                                ap.ProductId.ToString().ToLower() == (productId.ToLower()));
+        }
+
+        public Task<ApplicationUserProduct?> GetByCompositeKeyAsync(string userId, string productId)
+        {
+            return this
+                .GetAllAttached()
+                .SingleOrDefaultAsync(ap => ap.ApplicationUserId.ToLower() == (userId.ToLower()) &&
+                                ap.ProductId.ToString().ToLower() == (productId.ToLower()));
+        }
+    }
+}
---
+++ EventAgency.Data/Repository/Interfaces/ICartRepository.cs
@@ -0,0 +1,20 @@
+﻿using EventAgency.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventAgency.Data.Repository.Interfaces
+{
+    public interface ICartRepository : IRepository<ApplicationUserProduct, object>,
+        IAsyncRepository<ApplicationUserProduct, object>
+    {
+        ApplicationUserProduct? GetByCompositeKey(string userId, string productId);
+        Task<ApplicationUserProduct?> GetByCompositeKeyAsync(string userId, string productId);
+
+        bool Exists(string userId, string productId);
+
+        Task<bool> ExistsAsync(string userId, string productId);
+    }
+}
