4049a8d395e642000f769071f437352d8cf2385a
StefanyDimova <stefany.asenova@gmail.com>
2025-07-22 18:04:15 +0300

    Create Index View and ViewModel of Cart.

---
+++ EventAgency.Web.ViewModels/Cart/CartItemViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventAgency.Web.ViewModels.Cart
+{
+    public class CartItemViewModel
+    {
+        public string ProductId { get; set; } = null!;
+
+        public string ProductName { get; set; } = null!;
+
+        public string? ImageUrl { get; set; }
+
+        public decimal Price { get; set; }
+
+        public int Quantity { get; set; }
+
+
+    }
+}
